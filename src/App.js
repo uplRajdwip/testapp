abb1ebdb8361889b17abb05b2b29d3beedd76d5e
uplSnehasish <snehasish.mondal@utplco.com>
2022-12-21 10:20:07 +0530

    changes in the rater common component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import MuiRater from './common/MuiRater';
 
 function App() {
   return (
@@ -19,7 +18,6 @@ function App() {
           Learn React
         </a>
       </header> */}
-      <MuiRater/>
     </div>
   );
 }
